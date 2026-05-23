566243eaf354235b15c53ead3b96828be6bb3706
yasmeen-sigin <yasmeene781@gmail.com>
2026-05-23 17:59:03 +0300

    Added about page with name variable

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about',function(){
+    $name="Yasmeen";
+    $departments=[
+        '1'=> 'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales'
+     ];
+    
+    return view('about',data:compact('name','departments'));
+
+});
+
+Route::post('/about',function(){
+     $name= $_POST['name'];
+     $departments=[
+        '1'=> 'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales'
+     ];
+return view('about',data:compact('name','departments'));
+
+
+});
